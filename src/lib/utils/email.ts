3bb0fcc4654487656532ad45344df43ade7eba01
Szu Han <szuhan.eng@gmail.com>
2023-04-25 14:00:32 +1200

    fix/rename-env-variable

--- src/lib/utils/email.ts
+++ src/lib/utils/email.ts
@@ -1,10 +1,10 @@
 import * as SibApiV3Sdk from '@sendinblue/client'
 import {
-    SENDINBLUE_API_KEY
+    PRIVATE_SENDINBLUE_API_KEY
 } from '$env/static/private';
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi()
 const apiKey = apiInstance.authentications['apiKey'];
-apiKey.apiKey = SENDINBLUE_API_KEY
+apiKey.apiKey = PRIVATE_SENDINBLUE_API_KEY
 
 export async function sendEmail(recipientEmail = 'szuhan.eng@gmail.com', recipientName: string,fileURL: string | undefined, content: any, fileName: string) {
     const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
